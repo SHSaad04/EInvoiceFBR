ead296982bd4fb939c16477a43821f24853cb3d0
Saad Javed <saad.javed@iengineering.com>
2025-07-31 01:18:34 +0500

    Dto and mapping profile

---
+++ EInvoice.Common/DTO/Filter/FilterDTOBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EInvoice.Common.DTO.Filter
+{
+    public class FilterDTOBase
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string SearchQuery { get; set; } = string.Empty;
+        public string SortActive { get; set; } = string.Empty;
+        public string SortDirection { get; set; } = "asc";
+    }
+}
---
+++ EInvoice.Service/Helpers/MappingProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using EInvoice.Common.Entities;
+using EInvoice.Common.Pagination;
+using EInvoice.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EInvoice.Service.Helpers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<ClientDTO, Client>().ReverseMap();
+            CreateMap<PagedResult<ClientDTO>, PagedResult<Client>>().ReverseMap();
+
+            CreateMap<InvoiceDTO, Invoice>().ReverseMap();
+            CreateMap<PagedResult<InvoiceDTO>, PagedResult<Invoice>>().ReverseMap();
+
+            CreateMap<InvoiceItemDTO, InvoiceItem>().ReverseMap();
+            CreateMap<PagedResult<InvoiceItemDTO>, PagedResult<InvoiceItem>>().ReverseMap();
+
+            CreateMap<OrganizationDTO, Organization>().ReverseMap();
+            CreateMap<PagedResult<OrganizationDTO>, PagedResult<Organization>>().ReverseMap();
+
+            CreateMap<OrganizationDTO, Organization>().ReverseMap();
+            CreateMap<PagedResult<OrganizationDTO>, PagedResult<Organization>>().ReverseMap();
+
+            CreateMap<UserDTO, User>().ReverseMap();
+            CreateMap<PagedResult<UserDTO>, PagedResult<User>>().ReverseMap();
+        }
+    }
+}
