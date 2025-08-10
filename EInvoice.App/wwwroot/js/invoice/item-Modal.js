767d27f0561dec6959e119d17b6f43d4113cd42c
Saad Javed <saad.javed@iengineering.com>
2025-08-11 01:29:22 +0500

    add invoice success

--- EInvoice.App/wwwroot/js/invoice/item-Modal.js
+++ EInvoice.App/wwwroot/js/invoice/item-Modal.js
@@ -8,10 +8,10 @@
     });
 
     // Edit Item (when clicking edit buttons in the table)
-    $(document).on("click", ".edit-item", function () {
-        fillModal($(this).data());
-        itemModal.show();
-    });
+    //$(document).on("click", ".edit-item", function () {
+    //    fillModal($(this).data());
+    //    itemModal.show();
+    //});
 
     function clearModal() {
         $("#ProductId").val("");
@@ -20,17 +20,18 @@
         // Clear other fields if needed
     }
 
-    function fillModal(data) {
-        $("#ProductId").val(data.id);
-        $("#ProductName").val(data.name);
-        $("#ProductRate").val(data.rate);
-        // Map other fields if needed
-    }
+    //function fillModal(data) {
+    //    $("#ProductId").val(data.id);
+    //    $("#ProductName").val(data.name);
+    //    $("#ProductRate").val(data.rate);
+    //    // Map other fields if needed
+    //}
 
-    //Add Item click in modal
+    //Add Item
     let invoiceItems = [];
+    let editIndex = null;
     $("#addItemBtn").on("click", function () {
-        const item = {
+        const newItem = {
             ProductId: $("#InvoiceItem_Id").val(),
             HsCode: $("#InvoiceItem_HsCode").val(),
             ProductDescription: $("#ProductDescription").val(),
@@ -42,16 +43,24 @@
             FixedNotifiedValueOrRetailPrice: parseFloat($("#InvoiceItem_FixedNotifiedValueOrRetailPrice").val()) || 0,
             SalesTaxApplicable: parseFloat($("#InvoiceItem_SalesTaxApplicable").val()) || 0,
             SalesTaxWithheldAtSource: parseFloat($("#InvoiceItem_SalesTaxWithheldAtSource").val()) || 0,
-            ExtraTax: parseFloat($("#InvoiceItem_ExtraTax").val()) || null,
-            FurtherTax: parseFloat($("#InvoiceItem_FurtherTax").val()) || null,
-            SroScheduleNo: $("#InvoiceItem_SroScheduleNo").val(),
-            FedPayable: parseFloat($("#InvoiceItem_FedPayable").val()) || null,
-            Discount: parseFloat($("#InvoiceItem_Discount").val()) || null,
+            ExtraTax: parseFloat($("#InvoiceItem_ExtraTax").val()) || 0,
+            FurtherTax: parseFloat($("#InvoiceItem_FurtherTax").val()) || 0,
+            SroScheduleNo: $("#InvoiceItem_SroScheduleNo").val() || "",
+            FedPayable: parseFloat($("#InvoiceItem_FedPayable").val()) || 0,
+            Discount: parseFloat($("#InvoiceItem_Discount").val()) || 0,
             SaleType: $("#InvoiceItem_SaleType").val(),
-            SroItemSerialNo: $("#InvoiceItem_SroItemSerialNo").val()
+            SroItemSerialNo: $("#InvoiceItem_SroItemSerialNo").val() || ""
         };
 
-        invoiceItems.push(item);
+        if (editIndex !== null) {
+            // Update existing item
+            invoiceItems[editIndex] = newItem;
+            editIndex = null;
+            $('#addItemBtn').text('Add Item');
+        } else {
+            // Add new item
+            invoiceItems.push(newItem);
+        }
         renderInvoiceCards();
 
         $("#itemModal").modal("hide");
@@ -59,9 +68,9 @@
 
     // Render cards
     function renderInvoiceCards() {
-        const container = $("#invoiceItemsCards");
-        container.empty();
-        const template = $("#invoiceItemCardTemplate").html();
+        const template = $('#invoiceItemCardTemplate').html();
+        const $container = $('#invoiceItemsCards');
+        $container.empty();
 
         invoiceItems.forEach((item, index) => {
             let cardHtml = template
@@ -72,30 +81,68 @@
                 .replace(/{Rate}/g, item.Rate.toFixed(2))
                 .replace(/{TotalValue}/g, item.TotalValue.toFixed(2))
                 .replace(/{Index}/g, index);
-            container.append(cardHtml);
+
+            // wrap card and hidden fields together
+            const $wrapper = $('<div>').append(cardHtml);
+            $wrapper.append(`
+            <input type="hidden" name="InvoiceItems[${index}].ProductId" value="${item.ProductId}" />
+            <input type="hidden" name="InvoiceItems[${index}].HsCode" value="${item.HsCode}" />
+            <input type="hidden" name="InvoiceItems[${index}].ProductDescription" value="${item.ProductDescription}" />
+            <input type="hidden" name="InvoiceItems[${index}].Rate" value="${item.Rate}" />
+            <input type="hidden" name="InvoiceItems[${index}].UoM" value="${item.UoM}" />
+            <input type="hidden" name="InvoiceItems[${index}].Quantity" value="${item.Quantity}" />
+            <input type="hidden" name="InvoiceItems[${index}].TotalValue" value="${item.TotalValue}" />
+            <input type="hidden" name="InvoiceItems[${index}].ValueSalesExcludingST" value="${item.ValueSalesExcludingST}" />
+            <input type="hidden" name="InvoiceItems[${index}].FixedNotifiedValueOrRetailPrice" value="${item.FixedNotifiedValueOrRetailPrice}" />
+            <input type="hidden" name="InvoiceItems[${index}].SalesTaxApplicable" value="${item.SalesTaxApplicable}" />
+            <input type="hidden" name="InvoiceItems[${index}].SalesTaxWithheldAtSource" value="${item.SalesTaxWithheldAtSource}" />
+            <input type="hidden" name="InvoiceItems[${index}].ExtraTax" value="${item.ExtraTax}" />
+            <input type="hidden" name="InvoiceItems[${index}].FurtherTax" value="${item.FurtherTax}" />
+            <input type="hidden" name="InvoiceItems[${index}].SroScheduleNo" value="${item.SroScheduleNo}" />
+            <input type="hidden" name="InvoiceItems[${index}].FedPayable" value="${item.FedPayable}" />
+            <input type="hidden" name="InvoiceItems[${index}].Discount" value="${item.Discount}" />
+            <input type="hidden" name="InvoiceItems[${index}].SaleType" value="${item.SaleType}" />
+            <input type="hidden" name="InvoiceItems[${index}].SroItemSerialNo" value="${item.SroItemSerialNo}" />
+        `);
+
+            $container.append($wrapper);
         });
     }
 
-    // Handle remove
+    // Remove Item
     $(document).on("click", ".remove-item", function () {
         const index = $(this).data("index");
         invoiceItems.splice(index, 1);
         renderInvoiceCards();
     });
 
-    // Handle edit
+    // Edit Item
+
     $(document).on("click", ".edit-item", function () {
-        const index = $(this).data("index");
-        const item = invoiceItems[index];
+        editIndex = $(this).data('index');
+        const item = invoiceItems[editIndex];
         // populate modal fields with item values
+        $("#InvoiceItem_Id").val(item.ProductId);
+        $("#InvoiceItem_ProductId").val(item.ProductId);
         $("#InvoiceItem_HsCode").val(item.HsCode);
         $("#InvoiceItem_ProductDescription").val(item.ProductDescription);
         $("#InvoiceItem_Rate").val(item.Rate);
         $("#InvoiceItem_UoM").val(item.UoM);
         $("#InvoiceItem_Quantity").val(item.Quantity);
         $("#InvoiceItem_TotalValue").val(item.TotalValue);
+        $("#InvoiceItem_ValueSalesExcludingST").val(item.ValueSalesExcludingST);
+        $("#InvoiceItem_FixedNotifiedValueOrRetailPrice").val(item.FixedNotifiedValueOrRetailPrice);
+        $("#InvoiceItem_SalesTaxApplicable").val(item.SalesTaxApplicable);
+        $("#InvoiceItem_SalesTaxWithheldAtSource").val(item.SalesTaxWithheldAtSource);
+        $("#InvoiceItem_ExtraTax").val(item.ExtraTax);
+        $("#InvoiceItem_FurtherTax").val(item.FurtherTax);
+        $("#InvoiceItem_SroScheduleNo").val(item.SroScheduleNo);
+        $("#InvoiceItem_FedPayable").val(item.FedPayable);
+        $("#InvoiceItem_Discount").val(item.Discount);
+        $("#InvoiceItem_SaleType").val(item.SaleType);
+        $("#InvoiceItem_SroItemSerialNo").val(item.SroItemSerialNo);
         // set edit index
-        $("#itemModal").data("edit-index", index);
+        $("#itemModal").data("edit-index", editIndex);
         $("#itemModal").modal("show");
     });
 });
--- EInvoice.Common/DTO/InvoiceDTO.cs
+++ EInvoice.Common/DTO/InvoiceDTO.cs
@@ -56,7 +56,7 @@ namespace EInvoice.Common.DTO
         public long BuyerId { get; set; }
         public ClientDTO? Buyer { get; set; }
         public List<ClientDTO>? Clients { get; set; }
-        public ProductViewModel ProductViewModel { get; set; }
+        public ProductViewModel? ProductViewModel { get; set; }
         public List<InvoiceTypeDTO>? InvoiceTypes { get; set; }
         [MinLength(1, ErrorMessage = "At least one item is required")]
         public List<InvoiceItemDTO> InvoiceItems { get; set; } = new();
