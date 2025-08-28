73af9a1accd5d06517f47ecfbc9635717c9d4a20
Saad Javed <saad.javed@iengineering.com>
2025-08-29 02:15:10 +0500

    ui updates

---
+++ EInvoice.App/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using EInvoice.Common.ViewModel;
+using EInvoice.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace EInvoice.App.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
+    }
+}
